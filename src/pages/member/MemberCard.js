33eb7d1535784334c97609f932cf2072e41ccbfd
sharonttop <sharonttop@gmail.com>
2021-11-06 16:16:58 +0800

    Add package.json

--- src/pages/member/MemberCard.js
+++ src/pages/member/MemberCard.js
@@ -19,7 +19,7 @@ export default class MultipleItems extends Component {
         <link
           rel="stylesheet"
           type="text/css"
-          charset="UTF-8"
+          charSet="UTF-8"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
         />
         <link
